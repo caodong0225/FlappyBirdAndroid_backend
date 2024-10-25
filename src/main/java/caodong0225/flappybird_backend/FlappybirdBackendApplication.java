5f0ee9944191d03a254410db5ee5a3ac6701de50
jyzxc <867137498@qq.com>
2024-10-26 01:43:16 +0800

    feat: init commit

---
+++ src/main/java/caodong0225/flappybird_backend/FlappybirdBackendApplication.java
@@ -0,0 +1,13 @@
+package caodong0225.flappybird_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlappybirdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlappybirdBackendApplication.class, args);
+    }
+
+}
